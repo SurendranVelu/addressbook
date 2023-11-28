3b16948424c58b6d35a1dc7698e69011f566e9c3
surendran <surendran0505@gmail.com>
2023-11-28 14:17:25 +0530

    Added the server config file

--- server-config.sh
+++ server-config.sh
@@ -4,13 +4,13 @@ sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
 
-if[ -d "addressbook" ]
+if [ -d "addressbook" ]
 then
-    echo "repo is cloned and exist"
-    git pull origin master
+  echo "repo is cloned and repo exists"
+  cd /home/ec2-user/addressbook
+  git pull origin master
 else
     git clone https://github.com/SurendranVelu/addressbook.git
 fi
-cd /home/ec2-user/addressbook    
-
+cd /home/ec2-user/addressbook
 mvn package

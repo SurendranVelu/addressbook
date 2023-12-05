70d0f3def83083aa9fc0c5b3ec8accc9d692dbeb
root <root@ip-172-31-34-244.ap-south-1.compute.internal>
2023-12-05 02:41:36 +0000

    Updated docker file

--- server-config.sh
+++ server-config.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-sudo yum install java-1.8.0-openjdk-devel -y
+#sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
-sudo yum install maven -y
+sudo yum install docker
+sudo systemctl start docker
+#sudo yum install maven -y
 
 if [ -d "addressbook" ]
 then
@@ -11,6 +13,9 @@ then
   git pull origin master
 else
     git clone https://github.com/SurendranVelu/addressbook.git
+    cd /home/ec2-user/addressbook
+    git checkout master
 fi
-cd /home/ec2-user/addressbook
-mvn package
+#cd /home/ec2-user/addressbook
+#mvn package
+sudo docker build -t imagename /home/ec2-user/addressbook 
